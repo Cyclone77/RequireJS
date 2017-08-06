3720dd5b1af61294a4e555d62967e2f9853fca9f
cyclone77 <fillchar@live.com>
2017-08-06 22:32:59 +0800

    RequireJS学习

---
+++ houdunwang/resource/main.js
@@ -0,0 +1,31 @@
+require.config({
+    //基准路径
+    baseUrl: '../resource/app',
+    //模块路径
+    paths: {
+        'hd': 'hd',
+        'css': '../lib/css.min',
+        'jquery': '../lib/jquery.min',
+        'angular': '../lib/angular.min',
+        'bootstrap': '../lib/bootstrap.min',
+    },
+    //依赖关系
+    shim: {
+        'hd': {
+            // exports: 'modal',
+            init: function () {
+                return {
+                    modal: modal,
+                    success: success,
+                }
+            }
+        },
+        //houdunren.com
+        'bootstrap': {
+            'deps': ['jquery', 'css!../css/bootstrap.min.css', 'css!../css/font-awesome.min.css']
+        }
+    }
+});
+// require(['jquery', 'angular'], function ($, angular) {
+//     $('body').css({'backgroundColor': 'red'});
+// })
