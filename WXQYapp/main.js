a924899a97157c74de9b87cb5059dd4eba758bd7
cyclone77 <fillchar@live.com>
2017-09-03 18:42:13 +0800

    ..

--- WXQYapp/main.js
+++ WXQYapp/main.js
@@ -15,9 +15,6 @@
         'common': './../Resource/utils/common'
     },
     shim: {
-        'jquery': {
-            'deps': ["css!./../Resource/utils/common.css"]
-        },
         'bootstrap': {
             'deps': ['jquery', 'css!./../Resource/css/bootstrap.min.css']
         },
@@ -27,47 +24,49 @@
     }
 });
 
-require(['domReady', 'bootstrap', 'director', 'common'], function(domReady, bs, route, common) {
+require(['domReady', 'css!./../Resource/utils/common.css', 'bootstrap', 'director', 'common'],
+    function(domReady, commoncss, bs, route, common) {
 
-    domReady(function() {
-        console.log('dom加载完成！');
-    })
-
-    if (!location.hash) location.hash = "/";
-
-    var router = new route({
-        '/': function() {
-            common.updateTitle("运维平台", "四川广力软件科技有限公司-运维平台");
-        },
-        '/examine': intoExamine,
-        '/issues': intoIssues,
-        '/log': log
-    })
-    router.configure({
-        notfound: function() {
-            common.updateTitle("运维平台", "您所在访问的页面不存在！");
-        }
-    });
-    router.init();
-    router.on(['/', '/examine', '/issues'], function() {
-        $(".receive").empty();
-    })
-
-    function intoExamine() {
-        require(["examine"], function(examine) {
-            examine.init()
-        });
-    }
-
-    function intoIssues() {
-        require(["issues"], function(issues) {
-            issues.init()
-        });
-    }
-
-    function log() {
-        require(["log"], function(log) {
-            log.init();
+        domReady(function() {
+            console.log('dom加载完成！');
         })
-    }
-})
+
+        if (!location.hash) location.hash = "/";
+
+        var router = new route({
+            '/': function() {
+                common.updateTitle("运维平台", "四川广力软件科技有限公司-运维平台");
+            },
+            '/examine': intoExamine,
+            '/issues': intoIssues,
+            '/log': log
+        });
+
+        router.configure({
+            notfound: function() {
+                common.updateTitle("运维平台", "您所在访问的页面不存在！");
+            }
+        });
+        router.init();
+        router.on(['/', '/examine', '/issues'], function() {
+            $(".receive").empty();
+        })
+
+        function intoExamine() {
+            require(["examine"], function(examine) {
+                examine.init()
+            });
+        }
+
+        function intoIssues() {
+            require(["issues"], function(issues) {
+                issues.init()
+            });
+        }
+
+        function log() {
+            require(["log"], function(log) {
+                log.init();
+            })
+        }
+    })
